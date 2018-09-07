aef523dc481b7d9e272062f8b07fa6477ab89b5f
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-09-07 12:31:16 +0530

    Add files via upload

---
+++ intfloatarithmetic.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+   int a;
+   float b,c,d,e,f;
+   a=5;b=0.3f;c=4.5f;
+   d=a+b+c;
+   e=c-b;
+   f=c*a;
+   System.out.println(d+" "+e+" "+f);
+  }
+}
