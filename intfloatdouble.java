466463ead8079c8a324068d54d205f3a7f166f47
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-09-07 15:01:00 +0530

    Add files via upload

---
+++ intfloatdouble.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[]args) {
+    int a=5;
+    double b=0.5,e,d,g;
+    float c=0.25f;
+    d=b-a+c;
+    e=a*c/c;
+    g=b/c*a;
+    System.out.println(d+" "+e+" "+g);
+  }
+}
