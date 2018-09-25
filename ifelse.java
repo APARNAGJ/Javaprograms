23c97b946f39c8566e5021867a71114a18517166
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-09-25 15:14:40 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ public class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int DaysInTheatre;
+      DaysInTheatre=55;
+      if (DaysInTheatre>50)
+        System.out.println("Box Office Hit");
+      else 
+        System.out.println("Flop")
+	}
+}
