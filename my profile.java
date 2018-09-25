426b4b801e9793836340afcb86c3328d905af584
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-09-25 15:20:20 +0530

    Add files via upload

---
+++ my profile.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+    {
+       String name="Aparna";
+        String city="Chennai";
+        int age=21;
+        float height=5.1f;
+        boolean married=false;
+        char sex='F';
+        double city_population=4828853.00;
+ 
+        System.out.println("Name:" +name + "\n City:" +city + "\n Age:" +age + "\n Height:"+height +"\n Married:" +married +"\n Sex:" +sex +"\n City Population:" + city_population);
+ 
+	}
+}
