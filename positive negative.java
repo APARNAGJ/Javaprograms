de690a917ebdfa6b2911f85e07fd86b545131b7c
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-10-03 12:04:32 +0530

    Add files via upload

---
+++ positive negative.java
@@ -0,0 +1,12 @@
+public class PositiveNegative {
+  public static void main(String[] args) {
+    int num;
+    num=3;
+    if (num>0)
+    System.out.println("Num is Positive");
+    else if (num<0)
+    System.out.println("Num is Negative");
+    else 
+    System.out.println("Zero");
+ }
+}
