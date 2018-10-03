5dc40eefae822dcf5ac1fe60ba5da6c88aebd907
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-10-03 12:44:47 +0530

    Update and rename Coupon.java to GenerateCoupon.java

---
+++ GenerateCoupon.java
@@ -0,0 +1,17 @@
+ public class GenerateCoupon
+{
+	public static void main (String[] args)
+	{
+       int purchasedamount;
+      purchasedamount=15000;
+      if(purchasedamount>=20000)
+        System.out.println("Free Trolly Suitcase");
+     else if(purchasedamount>=10000)
+        System.out.println("Free Travel Bag");
+      else if(purchasedamount>=5000)
+        System.out.println("Free Back PacK");
+      else
+        System.out.println("Collect money for the given cover");
+      
+	}
+}
