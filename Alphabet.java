4b57bc5abb29895b2da11ef627ba88e90394ce08
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-10-04 12:19:45 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char ch = '*';
+
+        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            System.out.println(ch + " is an alphabet.");
+        else
+            System.out.println(ch + " is not an alphabet.");
+    }
+}
