840fcd761a2cb5bc03c9d661ed982a46001b0207
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2018-10-04 11:45:27 +0530

    Update and rename Oddeven.java to Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+public class Evenodd{
+
+     public static void main(String []args){
+      int n;
+      n=25;
+      if (n%2==0)
+      System.out.println("n is Even");
+      else
+      System.out.println("n is Odd");
+
+     }
+}
