22519e7b23539e78a990fe809942544aa1ef67b1
APARNAGJ <43058828+APARNAGJ@users.noreply.github.com>
2019-02-12 12:48:25 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    int n;
+    Scanner yr=new Scanner(System.in);
+    System.out.println("Enter the Year");
+    n=yr.nextInt();
+    {
+      if(n%4==0)
+    System.out.println("It is a Leap year");
+  }
+  }
+}
